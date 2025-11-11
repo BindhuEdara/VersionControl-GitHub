9ed55396108c3498232cfc26e0bbc0ba76265d7f
BindhuEdara <mandadi.bindu@gmail.com>
2025-11-11 17:14:48 +0530

    worked on Q11

---
+++ Q11/index.js
@@ -0,0 +1,10 @@
+let num=2
+
+function isPrime(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i < num; i++){
+    if(num%i==0) return false
+  }
+  return true
+}
+console.log(isPrime(num));
